23831672a79581313c56365b409f9cc031436adf
Sean DePotter <seandep@live.com>
2019-06-12 04:08:45 -0400

    Exercise 1

--- Working with Control Flow Exercises/Program.cs
+++ Working with Control Flow Exercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Working_with_Control_Flow_Exercises
 {
@@ -6,7 +7,34 @@ namespace Working_with_Control_Flow_Exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Exercise1();
+        }
+
+        private static void Exercise1()
+        {
+            /* Write a program to count how many numbers between 1 and 100
+             are divisible by 3 with no remainder.
+             Display the count on the console.
+             */
+
+            //added func displays which numbers are divisible by 3
+            var nonDivisibleNumbers = new List<int>();
+            var totalNonDivisiable = 0;
+
+            for (int i = 1; i < 100; i++)
+            {
+                if ((i%3) > 0)
+                {
+                    nonDivisibleNumbers.Add(i);
+                    totalNonDivisiable++;
+                }
+            }
+
+            Console.WriteLine(@"Total Non Divisible:" + totalNonDivisiable);
+            foreach (var nonDivisibleNumber in nonDivisibleNumbers)
+            {
+                Console.Write(nonDivisibleNumber + ",");
+            }
         }
     }
 }
